888bd247346198eeeec43640577292aa0532439e
meesalavamsi <156990795+meesalavamsi@users.noreply.github.com>
2025-07-18 09:27:36 +0530

    Create First_Program.java

---
+++ Day-02/First_Program.java
@@ -0,0 +1,15 @@
+/*Hello World!!
+Here, we have a simple Java code snippet where we have written a Hello World program.
+
+Do not worry about the syntax for now. Just observe that we are using the System.out.println statement to print a message on the screen.
+
+Try submitting the code and check if it runs successfully. In the upcoming pages, we will analyze its components, understand how Java processes it, and explore how compilation and execution work.**
+
+Fun Fact: Despite creating Java, James Gosling never patented it, meaning he didn’t personally profit as much as Sun Microsystems, which later got acquired by Oracle.*/
+
+class Codechef {
+    public static void main(String[] args) {
+	    // Printing a Hello World!! message
+        System.out.println("Hello World!!");   
+    }
+}
